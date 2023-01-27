66c927c3f053d289836b28befcb65d50a5a99026
RhettB1214 <102620185+RhettB1214@users.noreply.github.com>
2023-01-27 14:19:27 -0500

    Added
    Wrote Red and Blue Far side Translation Autons

--- 9623S Code/AutoTestCode1/src/auton.cpp
+++ 9623S Code/AutoTestCode1/src/auton.cpp
@@ -38,7 +38,27 @@ void redClose()
 //Red Far Side Auton Function
 void redFar()
 {
-
+    controller->setTarget({24_in, 0_in, 0_deg});
+    controller->waitUntilSettled();
+    controller->setTarget({0_in, -5_in, 0_deg});
+    controller->waitUntilSettled();
+    rollerMotor.moveVelocity(200);
+    pros::delay(500);
+    rollerMotor.moveVelocity(0);
+    controller->setTarget({0_in, 0_in, 45_deg});
+    controller->waitUntilSettled();
+    controller->setTarget({-48_in, 48_in, 0_deg});
+    controller->waitUntilSettled();
+    controller->setTarget({0_in, 0_in, -90_deg});
+    controller->waitUntilSettled();
+    shoot();
+    pros::delay(125);
+    load();
+    controller->setTarget({0_in, 0_in, 90_deg});
+    controller->waitUntilSettled();
+    controller->setTarget({-48_in, 48_in, 0_deg});
+    controller->waitUntilSettled();
+    controller->setTarget({0_in, 0_in, -135_deg});
 }
 
 //Blue Close Side Auton Function
@@ -69,7 +89,27 @@ void blueClose()
 //Blue Far Side Auton Function
 void blueFar()
 {
-
+    controller->setTarget({-24_in, 0_in, 0_deg});
+    controller->waitUntilSettled();
+    controller->setTarget({0_in, 5_in, 0_deg});
+    controller->waitUntilSettled();
+    rollerMotor.moveVelocity(200);
+    pros::delay(500);
+    rollerMotor.moveVelocity(0);
+    controller->setTarget({0_in, 0_in, 45_deg});
+    controller->waitUntilSettled();
+    controller->setTarget({48_in, -48_in, 0_deg});
+    controller->waitUntilSettled();
+    controller->setTarget({0_in, 0_in, -90_deg});
+    controller->waitUntilSettled();
+    shoot();
+    pros::delay(125);
+    load();
+    controller->setTarget({0_in, 0_in, 90_deg});
+    controller->waitUntilSettled();
+    controller->setTarget({48_in, -48_in, 0_deg});
+    controller->waitUntilSettled();
+    controller->setTarget({0_in, 0_in, -135_deg});
 }
 
 //Programming Skills Auton Function
