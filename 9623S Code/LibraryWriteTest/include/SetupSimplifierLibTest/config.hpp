7a21b29236320d8638e0b3cb10ffd513b21e84ea
RhettB1214 <102620185+RhettB1214@users.noreply.github.com>
2023-03-07 09:12:09 -0500

    LibWriteTest

---
+++ 9623S Code/LibraryWriteTest/include/SetupSimplifierLibTest/config.hpp
@@ -0,0 +1,116 @@
+#pragma once
+
+
+
+/*
+       MOTOR DEFINITIONS 
+    ----------------------
+    DEFINE ALL YOUR MOTOR
+    PORTS AND NAMES HERE
+    ----------------------
+    FOR ANY MOTORS THAT YOU
+    DO NOT HAVE, SIMPLY
+    USE "nullptr" FOR THE
+    PORT NUMBER
+    ----------------------
+*/
+#define MOTOR_1_PORT 1
+#define MOTOR_1_NAME "MOTOR_1"
+#define MOTOR_2_PORT 2
+#define MOTOR_2_NAME "MOTOR_2"
+#define MOTOR_3_PORT 3
+#define MOTOR_3_NAME "MOTOR_3"
+#define MOTOR_4_PORT 4
+#define MOTOR_4_NAME "MOTOR_4"
+#define MOTOR_5_PORT 5
+#define MOTOR_5_NAME "MOTOR_5"
+#define MOTOR_6_PORT 6
+#define MOTOR_6_NAME "MOTOR_6"
+#define MOTOR_7_PORT 7
+#define MOTOR_7_NAME "MOTOR_7"
+#define MOTOR_8_PORT 8
+#define MOTOR_8_NAME "MOTOR_8"
+
+
+/*
+    SENSOR DEFINITIONS 
+    ----------------------
+    DEFINE ALL YOUR SENSOR
+    PORTS AND NAMES HERE
+    ----------------------
+    FOR ANY SENSORS THAT YOU
+    DO NOT HAVE, SIMPLY
+    USE "nullptr" FOR THE
+    PORT NUMBER
+    ----------------------
+*/
+#define SENSOR_1_TYPE 
+#define SENSOR_1_PORT 9
+#define SENSOR_1_NAME "SENSOR_1"
+#define SENSOR_2_PORT 10
+#define SENSOR_2_NAME "SENSOR_2"
+#define SENSOR_3_PORT 11
+#define SENSOR_3_NAME "SENSOR_3"
+#define SENSOR_4_PORT 12
+#define SENSOR_4_NAME "SENSOR_4"
+#define SENSOR_5_PORT 13
+#define SENSOR_5_NAME "SENSOR_5"
+#define SENSOR_6_PORT 14
+#define SENSOR_6_NAME "SENSOR_6"
+#define SENSOR_7_PORT 15
+#define SENSOR_7_NAME "SENSOR_7"
+#define SENSOR_8_PORT 16
+#define SENSOR_8_NAME "SENSOR_8"
+#define SENSOR_9_PORT 17
+#define SENSOR_9_NAME "SENSOR_9"
+#define SENSOR_10_PORT 18
+#define SENSOR_10_NAME "SENSOR_10"
+#define SENSOR_11_PORT 19
+#define SENSOR_11_NAME "SENSOR_11"
+#define SENSOR_12_PORT 20
+#define SENSOR_12_NAME "SENSOR_12"
+#define SENSOR_13_PORT 21
+#define SENSOR_13_NAME "SENSOR_13"
+
+
+/*
+    ADI DEFINITIONS 
+    ----------------------
+    DEFINE ALL YOUR ADI
+    PORTS AND NAMES HERE
+    ----------------------
+    FOR ANY ADI DEVICES THAT YOU
+    DO NOT HAVE, SIMPLY
+    USE "nullptr" FOR THE
+    PORT NUMBER
+    ----------------------
+    TYPES OF ADI DEVICES:
+    ADIAnalogIn
+    ADIAnalogOut
+    ADIDigitalIn
+    ADIDigitalOut
+    ADIEncoder
+    ADIGyro
+    ADIMotor
+    ADIPotentiometer
+    ADIUltrasonic
+    ADILed
+    ----------------------
+*/
+#define ADI_1_TYPE 'ADIAnalogIn'
+#define ADI_1_PORT 'A'
+#define ADI_1_NAME "ADI_1"
+#define ADI_2_PORT 'B'
+#define ADI_2_NAME "ADI_2"
+#define ADI_3_PORT 'C'
+#define ADI_3_NAME "ADI_3"
+#define ADI_4_PORT 'D'
+#define ADI_4_NAME "ADI_4"
+#define ADI_5_PORT 'E'
+#define ADI_5_NAME "ADI_5"
+#define ADI_6_PORT 'F'
+#define ADI_6_NAME "ADI_6"
+#define ADI_7_PORT 'G'
+#define ADI_7_NAME "ADI_7"
+#define ADI_8_PORT 'H'
+#define ADI_8_NAME "ADI_8"
---
+++ 9623S Code/LibraryWriteTest/include/SetupSimplifierLibTest/deviceSetup.hpp
@@ -0,0 +1,20 @@
+#include "config.hpp"
+#include "main.h"
+
+//MOTOR INITIALIZATION
+okapi::Motor MOTOR_1_NAME(MOTOR_1_PORT);
+okapi::Motor MOTOR_2_NAME(MOTOR_2_PORT);
+okapi::Motor MOTOR_3_NAME(MOTOR_3_PORT);
+okapi::Motor MOTOR_4_NAME(MOTOR_4_PORT);
+okapi::Motor MOTOR_5_NAME(MOTOR_5_PORT);
+okapi::Motor MOTOR_6_NAME(MOTOR_6_PORT);
+okapi::Motor MOTOR_7_NAME(MOTOR_7_PORT);
+okapi::Motor MOTOR_8_NAME(MOTOR_8_PORT);
+
+
+//SENSOR INITIALIZATION
+pros::ADI_1_TYPE ADI_1_NAME(ADI_1_PORT);
+
+
+
+
